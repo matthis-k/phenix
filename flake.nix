84cb81c6ecaacea9eb24899f96e45a9bf4eb75e5
matthis-k <matthis.kaelble@gmail.com>
2026-06-25 13:49:20 +0200

    Use GitHub URLs for subflake inputs (path: doesn't work with submodules in Nix)

--- flake.nix
+++ flake.nix
@@ -2,26 +2,21 @@
   description = "Phenix workspace superflake aggregating all subflakes";
 
   inputs = {
-    phenix-pins.url = "path:./phenix-pins";
+    phenix-pins.url = "github:matthis-k/phenix-pins";
 
-    phenix-packages.url = "path:./phenix-packages";
+    phenix-packages.url = "github:matthis-k/phenix-packages";
     phenix-packages.inputs.phenix-pins.follows = "phenix-pins";
 
-    phenix-shell.url = "path:./phenix-shell";
+    phenix-shell.url = "github:matthis-k/phenix-shell";
     phenix-shell.inputs.phenix-pins.follows = "phenix-pins";
-    phenix-shell.inputs.phenix-packages.follows = "phenix-packages";
 
-    phenix-nvim.url = "path:./phenix-nvim";
+    phenix-nvim.url = "github:matthis-k/phenix-nvim";
     phenix-nvim.inputs.phenix-pins.follows = "phenix-pins";
-    phenix-nvim.inputs.phenix-packages.follows = "phenix-packages";
 
-    phenix-hosts.url = "path:./phenix-hosts";
+    phenix-hosts.url = "github:matthis-k/phenix-hosts";
     phenix-hosts.inputs.phenix-pins.follows = "phenix-pins";
-    phenix-hosts.inputs.phenix-packages.follows = "phenix-packages";
-    phenix-hosts.inputs.phenix-shell.follows = "phenix-shell";
-    phenix-hosts.inputs.phenix-nvim.follows = "phenix-nvim";
 
-    phenix-tools.url = "path:./phenix-tools";
+    phenix-tools.url = "github:matthis-k/phenix-tools";
     phenix-tools.inputs.phenix-pins.follows = "phenix-pins";
   };
 
