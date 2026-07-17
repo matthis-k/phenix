060a4c98f38ed06993e670c2d7345a0455320052
Matthis Kälble <69082876+matthis-k@users.noreply.github.com>
2026-07-17 19:51:58 +0200

    test: validate root workspace inventory integration

--- workspace-apps.nix
+++ workspace-apps.nix
@@ -16,9 +16,20 @@
           type = "app";
           program = "${wrapper}/bin/${name}";
         };
+      inventoryCheck = pkgs.runCommand "phenix-workspace-inventory" {
+        nativeBuildInputs = [ workspace ];
+      } ''
+        export HOME="$TMPDIR"
+        phenix-workspace --root ${inputs.self} init --dry-run > "$out"
+        grep -q 'phenix-stitch' "$out"
+        grep -q 'phenix-tools' "$out"
+      '';
     in
     {
-      checks.phenix-workspace-package = workspace;
+      checks = {
+        phenix-workspace-package = workspace;
+        phenix-workspace-inventory = inventoryCheck;
+      };
 
       apps = {
         init-workspace = app "init-workspace" "init";
